a55c08e91a187b023dd862fcf0c7fd92036bce8f
salmanmaq <salmanmaq@gmail.com>
2018-02-18 06:10:41 +0500

    Added finetuning script

---
+++ finetuneMiccaiSeg.sh
@@ -0,0 +1,1 @@
+python mainMiccaiSeg.py --save-dir=/media/salman/DATA/miccaiSegResults --batchSize 1 --lr 0.0001 --epochs 2000 --saveTest True --resume save_MiccaiRecon/checkpoint_0.tar |& tee -a log_MiccaiSeg
